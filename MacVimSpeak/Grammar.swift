9388e988a1649e7ade79e2a4e4258cb8b786a1eb
pselle <pamela.selle@gmail.com>
2015-03-05 16:53:50 -0500

    Saving a progress on the other data structure

--- MacVimSpeak/Grammar.swift
+++ MacVimSpeak/Grammar.swift
@@ -15,6 +15,24 @@ struct Word {
         self.keystrokes = keystrokes
     }
 }
+
+struct MWord {
+    let say:String, keyCodes:[KeyCode]
+    init(say:String, keystrokes:String, afterMode: Any?) {
+        self.say = say
+        self.keyCodes = stringToKeyCodes(keystrokes)
+    }
+}
+
+func stringToKeyCodes(string:String) -> [KeyCode] {
+    var keyCodes = [CGKeyCode]()
+    for letter in string {
+        let strLetter = String(letter)
+        keyCodes.append(KeyCodeDict[strLetter])
+        println(KeyCodeDict[strLetter])
+    }
+    return keyCodes
+}
 typealias Choice = [Any]
 typealias Sequence = Array<Any>
 
@@ -64,32 +82,32 @@ let nonCountedAction = [
 //    Sequence [Word ("mark",           "m",   Some "prompt: marked %s"); char]
 //    Sequence [Word ("record-macro",   "q",   Some "prompt: recording %s"); char]
 //    Sequence [Word ("replace",        "r",   nil); char]
-    Word(say: "display-current-line-number", keystrokes: "<C-g>", afterMode: nil),
-    Word(say: "delete-remaining-line", keystrokes: "D", afterMode: nil),
-    Word(say: "change-remaining-line", keystrokes: "C", afterMode:      Some "insert"),
-    Word(say: "change-line", keystrokes: "cc", afterMode:     Some "insert"),
-    Word(say: "duplicate-line", keystrokes: "yyp", afterMode:    nil),
-    Word(say: "swap-characters", keystrokes: "xp", afterMode:     nil),
-    Word(say: "swap-words", keystrokes: "dwwP", afterMode:   nil),
-    Word(say: "swap-lines", keystrokes: "ddp", afterMode:    nil),
-    Word(say: "stop-recording", keystrokes: "q", afterMode:      Some "prompt: stopped"),
-    Word(say: "replace-mode", keystrokes: "R", afterMode:      Some "replace"),
-    Word(say: "overwrite", keystrokes: "R", afterMode:      Some "replace"),
-    Word(say: "visual", keystrokes: "v", afterMode:      Some "visual"),
-    Word(say: "select", keystrokes: "v", afterMode:      Some "visual"),
-    Word(say: "visual-line", keystrokes: "V", afterMode:      Some "visual"),
-    Word(say: "select-line", keystrokes: "V", afterMode:      Some "visual"),
-    Word(say: "visual-all", keystrokes: "ggVG", afterMode:   Some "visual"),
-    Word(say: "select-all", keystrokes: "ggVG", afterMode:   Some "visual"),
-    Word(say: "visual-block", keystrokes: "<C-v>", afterMode:  Some "visual"),
-    Word(say: "select-block", keystrokes: "<C-v>", afterMode:  Some "visual"),
-    Word(say: "scroll-top", keystrokes: "zt", afterMode:     nil),
-    Word(say: "scroll-middle", keystrokes: "zz", afterMode:     nil),
-    Word(say: "scroll-botton", keystrokes: "zb", afterMode:     nil),
-    Word(say: "scroll-top-reset-cursor", keystrokes: "z<enter>", afterMode:  nil),
-    Word(say: "scroll-middle-reset-cursor", keystrokes: ["z.", afterMode:         nil),
-    Word(say: "scroll-botton-reset-cursor", keystrokes: ["z-", afterMode:         nil)]
-    Word(say: \1, keystrokes: \2, afterMode: \3)
+    MWord(say: "display-current-line-number", keystrokes: "<C-g>", afterMode: nil)]
+//    Word(say: "delete-remaining-line", keystrokes: "D", afterMode: nil),
+//    Word(say: "change-remaining-line", keystrokes: "C", afterMode:      Some "insert"),
+//    Word(say: "change-line", keystrokes: "cc", afterMode:     Some "insert"),
+//    Word(say: "duplicate-line", keystrokes: "yyp", afterMode:    nil),
+//    Word(say: "swap-characters", keystrokes: "xp", afterMode:     nil),
+//    Word(say: "swap-words", keystrokes: "dwwP", afterMode:   nil),
+//    Word(say: "swap-lines", keystrokes: "ddp", afterMode:    nil),
+//    Word(say: "stop-recording", keystrokes: "q", afterMode:      Some "prompt: stopped"),
+//    Word(say: "replace-mode", keystrokes: "R", afterMode:      Some "replace"),
+//    Word(say: "overwrite", keystrokes: "R", afterMode:      Some "replace"),
+//    Word(say: "visual", keystrokes: "v", afterMode:      Some "visual"),
+//    Word(say: "select", keystrokes: "v", afterMode:      Some "visual"),
+//    Word(say: "visual-line", keystrokes: "V", afterMode:      Some "visual"),
+//    Word(say: "select-line", keystrokes: "V", afterMode:      Some "visual"),
+//    Word(say: "visual-all", keystrokes: "ggVG", afterMode:   Some "visual"),
+//    Word(say: "select-all", keystrokes: "ggVG", afterMode:   Some "visual"),
+//    Word(say: "visual-block", keystrokes: "<C-v>", afterMode:  Some "visual"),
+//    Word(say: "select-block", keystrokes: "<C-v>", afterMode:  Some "visual"),
+//    Word(say: "scroll-top", keystrokes: "zt", afterMode:     nil),
+//    Word(say: "scroll-middle", keystrokes: "zz", afterMode:     nil),
+//    Word(say: "scroll-botton", keystrokes: "zb", afterMode:     nil),
+//    Word(say: "scroll-top-reset-cursor", keystrokes: "z<enter>", afterMode:  nil),
+//    Word(say: "scroll-middle-reset-cursor", keystrokes: ["z.", afterMode:         nil),
+//    Word(say: "scroll-botton-reset-cursor", keystrokes: ["z-", afterMode:         nil)]
+//    Word(say: \1, keystrokes: \2, afterMode: \3)
 
 //Word \((.*?),\s*?(.*?),\s*?(.*?)\)
 

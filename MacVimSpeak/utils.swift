1b340342a7ef176d96b5790c2676b322916c3bed
pselle <pamela.selle@gmail.com>
2015-03-16 11:22:52 -0400

    Add VoiceCommands struct, organize a bit
    Attempting to clean up the project a little

---
+++ MacVimSpeak/utils.swift
@@ -0,0 +1,17 @@
+//
+//  utils.swift
+//  MacVimSpeak
+//
+//  Created by Pam Selle on 3/16/15.
+//  Copyright (c) 2015 The Webivore. All rights reserved.
+//
+
+import Foundation
+
+func mapDict<S, T, U>(myDict:[S:T], f: T -> U) -> [S:U] {
+    var newDict = [S: U]()
+    for (key, val) in myDict {
+        newDict[key] = f(val)
+    }
+    return newDict
+}
